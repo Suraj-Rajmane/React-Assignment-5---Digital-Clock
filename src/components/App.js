534528088f4c92b04ebc6c75c101d07a5b81b3ef
suraj009 <surajrajmane18@gmail.com>
2021-03-02 12:03:04 +0530

    first commit clock works fine

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,39 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            date : new Date()
+        }
+
+        this.tick = this.tick.bind(this);
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(this.tick, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        // console.log("state is not updated")
+        this.setState({
+            date: new Date()
+        }, 
+        // () => console.log("state is updated")
+        )
+    }
+    
     render() {
 
         return(
-            <>
-               
-            </>
+            <div className="Clock">
+               <h3 id="time">{this.state.date.toLocaleTimeString()}</h3>
+            </div>
         )
     }
 }
